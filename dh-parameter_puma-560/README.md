c56736940895350deadb6dff2844029dbccd8a06
Ashok Arora <arora8715@gmail.com>
2021-04-09 13:47:04 +0400

    Adds inverse kinematics

--- dh-parameter_puma-560/README.md
+++ dh-parameter_puma-560/README.md
@@ -2,19 +2,71 @@
 
 <h2 align="center">BCCS-9402</h2>
 
-Course Instructor: Dr. Vijay Bhaskar Semwal 
+__Course Instructor__: Dr. Vijay Bhaskar Semwal 
 
-Submitted by: Ashok Arora (2019BCS-075)
+__Submitted by__: Ashok Arora (2019BCS-075)
 
-Problem Statement: Write the code to calculate forward kinematics for PUMA 560 robot using DH table.
+__Problem Statement__: Write the code to calculate forward kinematics for PUMA 560 robot using DH table.
 
-Sample input/output:
-![Sample input-output](sample-io.png?raw=true)
+__Sample input/output (forward kinematics)__:
+```
+Program to calculate forward and inverse kinematics of PUMA 560 robot.
 
-The sample input is stored in `input.txt` file.
+Enter 1 for forward kinematics or 2 for inverse kinematics. Enter your choice: 1
+Enter parameters (a, alpha, d, theta) of link 0: 0, 0, 0, 10
+Enter parameters (a, alpha, d, theta) of link 1: 0, -90, 0, 15
+Enter parameters (a, alpha, d, theta) of link 2: 3, 0, 3, 20
+Enter parameters (a, alpha, d, theta) of link 3: 3, -90, 3, 25
+Enter parameters (a, alpha, d, theta) of link 4: 0, 90, 0, 30
+Enter parameters (a, alpha, d, theta) of link 5: 0, -90, 0, 35
+
+End effector position: (3.058, 3.586, -4.955)
+```
+
+__Sample input/output (inverse kinematics)__:
+```
+Program to calculate forward and inverse kinematics of PUMA 560 robot.
+
+Enter 1 for forward kinematics or 2 for inverse kinematics. Enter your choice: 2
+Enter robot features (a2, a3, d3, d4): 3, 3, 3, 3
+Enter 4x4 Transformation Matrix: 
+0.23409161, -0.38798654, -0.89144129,  3.05834348
+-0.7910126, -0.60910276,  0.05738376,  3.58554831
+-0.56524347, 0.69170823, -0.44948808, -4.95464258
+0,           0,           0,           1
+
+Possible joint angles are: 
+theta1: 10.000, theta2: 15.000, theta3: 20.000, theta4: 25.000, theta5: 28.801, theta6: 35.006
+
+theta1: -90.926, theta2: 29.337, theta3: 20.000, theta4: -109.328, theta5: 70.989, theta6: 104.001
+
+theta1: 10.000, theta2: 82.302, theta3: -110.000, theta4: 12.199, theta5: 90.316, theta6: 57.058
+
+theta1: -90.926, theta2: 150.663, theta3: -110.000, theta4: -106.264, theta5: 68.330, theta6: 95.194
+
+theta1: 10.000, theta2: 15.000, theta3: 20.000, theta4: 205.000, theta5: -28.801, theta6: 215.006
+
+theta1: -90.926, theta2: 29.337, theta3: 20.000, theta4: 70.672, theta5: -70.989, theta6: 284.001
+
+theta1: 10.000, theta2: 82.302, theta3: -110.000, theta4: 192.199, theta5: -90.316, theta6: 237.058
+
+theta1: -90.926, theta2: 150.663, theta3: -110.000, theta4: 73.736, theta5: -68.330, theta6: 275.194
+```
+
+### Note: 
+
+The sample input is stored in `input_fk.txt` and `input_ik.txt` files for forward and inverse kinematics respectively.
 
 A way to run the program without manually supplying the inputs every time:
 
 ```
-/usr/bin/python3.9 /path/to/main.py < input.txt
+/usr/bin/python3.9 /path/to/main.py < input_fk.txt
 ```
+
+or 
+
+```
+/usr/bin/python3.9 /path/to/main.py < input_ik.txt
+```
+
+
--- dh-parameter_puma-560/main.py
+++ dh-parameter_puma-560/main.py
@@ -1,36 +1,266 @@
 import math
 import numpy as np
-from math import cos
-from math import sin
+from math import cos, sin, atan2, degrees, radians
 
-def solve(dh_table):
 
-    def T(i, j):
-        theta_i = math.radians(dh_table[i][1])
-        a_j = dh_table[j][2]
-        alpha_j = math.radians(dh_table[j][3])
-        d_i = dh_table[i][0]
-        return np.array([[cos(theta_i), -sin(theta_i), 0, a_j],
-                        [cos(alpha_j) * sin(theta_i), cos(alpha_j) * cos(theta_i), -sin(alpha_j), -d_i * sin(alpha_j)],
-                        [sin(alpha_j) * sin(theta_i), sin(alpha_j) * cos(theta_i), cos(alpha_j), d_i * cos(alpha_j)],
-                        [0, 0, 0, 1]])
-
-    def final_transformation_matrix():
-        mat = T(1, 0)
-        for i in range(2, 6):
-            mat = np.matmul(mat, T(i, i - 1))
-        return mat
-
-    point_coords = np.array([0, 0, 0, 1])
-    final = np.round(np.matmul(final_transformation_matrix(), point_coords), 3) + 0
-    print("\nPosition of end effector is:", final)
-
-def main():
+def input_fk():
     dh_table = np.zeros((6, 4))
     for i in range(6):
-        parameters = np.array(list(map(int, input("Enter parameters of link " + str(i) + ": ").strip().split(','))))
+        parameters = np.array(list(map(int, input(
+            "Enter parameters (a, alpha, d, theta) of link " + str(i) + ": ").strip().split(','))))
         dh_table[i] = parameters
-    solve(dh_table)
+    return dh_table
+
+
+def solve_fk(dh_table):
+
+    def T(dh_table, i):
+        a_i1 = dh_table[i][0]
+        alpha_i1 = radians(dh_table[i][1])
+        d_i = dh_table[i][2]
+        theta_i = radians(dh_table[i][3])
+
+        return np.array([[cos(theta_i), -sin(theta_i), 0, a_i1],
+                         [cos(alpha_i1)*sin(theta_i), cos(alpha_i1) *
+                          cos(theta_i), -sin(alpha_i1), -d_i*sin(alpha_i1)],
+                         [sin(alpha_i1)*sin(theta_i), sin(alpha_i1) *
+                          cos(theta_i), cos(alpha_i1), d_i*cos(alpha_i1)],
+                         [0, 0, 0, 1]])
+
+    T_final = T(dh_table, 0)
+    for i in range(1, dh_table.shape[0]):
+        T_final = T_final @ T(dh_table, i)
+    end_effector_position = T_final @ np.array([0, 0, 0, 1])
+
+    print('\nEnd effector position: ({:.3f}, {:.3f}, {:.3f})'.format(
+        end_effector_position[0], end_effector_position[1], end_effector_position[2]))
+
+
+def input_ik():
+    inp = list(
+        map(int, input("Enter robot features (a2, a3, d3, d4): ").strip().split(',')))
+    T = np.zeros((4, 4))
+    print('Enter 4x4 Transformation Matrix: ')
+    for i in range(4):
+        T[i] = np.array(list(map(float, input().split(','))))
+
+    return (T, inp[0], inp[1], inp[2], inp[3])
+
+
+def solve_ik(T, a2, a3, d3, d4):
+    px = T[0, 3]
+    py = T[1, 3]
+    pz = T[2, 3]
+
+    # calculating theta1
+    theta1 = []
+    theta1.append(degrees(atan2(py, px) -
+                          atan2(d3, (px**2+py**2-d3**2)**0.5)))
+    theta1.append(degrees(atan2(py, px) -
+                          atan2(d3, -((px**2+py**2-d3**2)**0.5))))
+
+    K = (px**2+py**2+pz**2-a2**2-a3**2-d3**2-d4**2)/(2*a2)
+
+    # calculating theta3
+    theta3 = []
+    theta3.append(degrees(atan2(a3, d4) -
+                          atan2(K, (a3**2+d4**2-K**2)**0.5)))
+    theta3.append(degrees(atan2(a3, d4) -
+                          atan2(K, -((a3**2+d4**2-K**2)**0.5))))
+
+    # calculating theta2
+    theta2 = []
+
+    s1 = sin(radians(theta1[0]))
+    c1 = cos(radians(theta1[0]))
+    s3 = sin(radians(theta3[0]))
+    c3 = cos(radians(theta3[0]))
+    theta23 = degrees(atan2(
+        ((-a3-(a2*c3))*pz)-(((c1*px)+(s1*py))*(d4-(a2*s3))), (((a2*s3)-d4)*pz)+((a3+(a2*c3))*((c1*px)+(s1*py)))))
+    theta2.append(theta23-theta3[0])
+    s1 = sin(radians(theta1[1]))
+    c1 = cos(radians(theta1[1]))
+    s3 = sin(radians(theta3[0]))
+    c3 = cos(radians(theta3[0]))
+    theta23 = degrees(atan2(
+        ((-a3-a2*c3)*pz)-((c1*px+s1*py)*(d4-a2*s3)), ((a2*s3-d4)*pz)-((a3+a2*c3)*(c1*px+s1*py))))
+    theta2.append(theta23-theta3[0])
+    s1 = sin(radians(theta1[0]))
+    c1 = cos(radians(theta1[0]))
+    s3 = sin(radians(theta3[1]))
+    c3 = cos(radians(theta3[1]))
+    theta23 = degrees(atan2(
+        ((-a3-a2*c3)*pz)-((c1*px+s1*py)*(d4-a2*s3)), ((a2*s3-d4)*pz)-((a3+a2*c3)*(c1*px+s1*py))))
+    theta2.append(theta23-theta3[1])
+    s1 = sin(radians(theta1[1]))
+    c1 = cos(radians(theta1[1]))
+    s3 = sin(radians(theta3[1]))
+    c3 = cos(radians(theta3[1]))
+    theta23 = degrees(atan2(
+        ((-a3-a2*c3)*pz)-((c1*px+s1*py)*(d4-a2*s3)), ((a2*s3-d4)*pz)-((a3+a2*c3)*(c1*px+s1*py))))
+    theta2.append(theta23-theta3[1])
+
+    r13 = T[0, 2]
+    r23 = T[1, 2]
+    r33 = T[2, 2]
+
+    # calculating theta4
+    theta4 = []
+
+    s1 = sin(radians(theta1[0]))
+    c1 = cos(radians(theta1[0]))
+    s23 = sin(radians(theta2[0]+theta3[0]))
+    c23 = cos(radians(theta2[0]+theta3[0]))
+    theta4.append(degrees(atan2((-r13*s1)+(r23*c1),
+                                (-r13*c1*c23)-(r23*s1*c23)+(r33*s23))))
+    s1 = sin(radians(theta1[1]))
+    c1 = cos(radians(theta1[1]))
+    s23 = sin(radians(theta2[1]+theta3[0]))
+    c23 = cos(radians(theta2[1]+theta3[0]))
+    theta4.append(degrees(atan2((-r13*s1)+(r23*c1),
+                                (-r13*c1*c23)-(r23*s1*c23)+(r33*s23))))
+    s1 = sin(radians(theta1[0]))
+    c1 = cos(radians(theta1[0]))
+    s23 = sin(radians(theta2[2]+theta3[1]))
+    c23 = cos(radians(theta2[2]+theta3[1]))
+    theta4.append(degrees(atan2((-r13*s1)+(r23*c1),
+                                (-r13*c1*c23)-(r23*s1*c23)+(r33*s23))))
+    s1 = sin(radians(theta1[1]))
+    c1 = cos(radians(theta1[1]))
+    s23 = sin(radians(theta2[3]+theta3[1]))
+    c23 = cos(radians(theta2[3]+theta3[1]))
+    theta4.append(degrees(atan2((-r13*s1)+(r23*c1),
+                                (-r13*c1*c23)-(r23*s1*c23)+(r33*s23))))
+
+    # calculating theta5
+    theta5 = []
+
+    s1 = sin(radians(theta1[0]))
+    c1 = cos(radians(theta1[0]))
+    s23 = sin(radians(theta2[0]+theta3[0]))
+    c23 = cos(radians(theta2[0]+theta3[0]))
+    s4 = sin(radians(theta4[0]))
+    c4 = cos(radians(theta4[0]))
+    theta5.append(degrees(atan2((-r13*(c1*c23*c4+s1*s4)) -
+                                (r23*(s1*c23*c4))+(r33*s23*c4), (-r13*c1*s23)-(r23*s1*s23)-(r33*c23))))
+
+    s1 = sin(radians(theta1[1]))
+    c1 = cos(radians(theta1[1]))
+    s23 = sin(radians(theta2[1]+theta3[0]))
+    c23 = cos(radians(theta2[1]+theta3[0]))
+    s4 = sin(radians(theta4[1]))
+    c4 = cos(radians(theta4[1]))
+    theta5.append(degrees(atan2((-r13*(c1*c23*c4+s1*s4)) -
+                                (r23*(s1*c23*c4))+(r33*s23*c4), (-r13*c1*s23)-(r23*s1*s23)-(r33*c23))))
+
+    s1 = sin(radians(theta1[0]))
+    c1 = cos(radians(theta1[0]))
+    s23 = sin(radians(theta2[2]+theta3[1]))
+    c23 = cos(radians(theta2[2]+theta3[1]))
+    s4 = sin(radians(theta4[2]))
+    c4 = cos(radians(theta4[2]))
+    theta5.append(degrees(atan2((-r13*(c1*c23*c4+s1*s4)) -
+                                (r23*(s1*c23*c4))+(r33*s23*c4), (-r13*c1*s23)-(r23*s1*s23)-(r33*c23))))
+
+    s1 = sin(radians(theta1[1]))
+    c1 = cos(radians(theta1[1]))
+    s23 = sin(radians(theta2[3]+theta3[1]))
+    c23 = cos(radians(theta2[3]+theta3[1]))
+    s4 = sin(radians(theta4[3]))
+    c4 = cos(radians(theta4[3]))
+    theta5.append(degrees(atan2((-r13*(c1*c23*c4+s1*s4)) -
+                                (r23*(s1*c23*c4))+(r33*s23*c4), (-r13*c1*s23)-(r23*s1*s23)-(r33*c23))))
+
+    r11 = T[0, 0]
+    r21 = T[1, 0]
+    r31 = T[2, 0]
+
+    # calculating theta6
+    theta6 = []
+
+    s1 = sin(radians(theta1[0]))
+    c1 = cos(radians(theta1[0]))
+    s23 = sin(radians(theta2[0]+theta3[0]))
+    c23 = cos(radians(theta2[0]+theta3[0]))
+    s4 = sin(radians(theta4[0]))
+    c4 = cos(radians(theta4[0]))
+    s5 = sin(radians(theta5[0]))
+    c5 = cos(radians(theta5[0]))
+    theta6.append(degrees(atan2(
+        (-r11*(c1*c23*s4-s1*c4)-(r21*(s1*c23*s4+c1*c4))+r31*s23*s4), (r11*((c1*c23*c4+s1*s4)*c5-c1*s23*s5))+(r21*((s1*c23*c4-c1*s4)*c5-s1*s23*s5))-(r31*(s23*c4*c5+c23*s5)))))
+    s1 = sin(radians(theta1[1]))
+    c1 = cos(radians(theta1[1]))
+    s23 = sin(radians(theta2[1]+theta3[0]))
+    c23 = cos(radians(theta2[1]+theta3[0]))
+    s4 = sin(radians(theta4[1]))
+    c4 = cos(radians(theta4[1]))
+    s5 = sin(radians(theta5[1]))
+    c5 = cos(radians(theta5[1]))
+    theta6.append(degrees(atan2(
+        (-r11*(c1*c23*s4-s1*c4)-(r21*(s1*c23*s4+c1*c4))+r31*s23*s4), (r11*((c1*c23*c4+s1*s4)*c5-c1*s23*s5))+(r21*((s1*c23*c4-c1*s4)*c5-s1*s23*s5))-(r31*(s23*c4*c5+c23*s5)))))
+    s1 = sin(radians(theta1[0]))
+    c1 = cos(radians(theta1[0]))
+    s23 = sin(radians(theta2[2]+theta3[1]))
+    c23 = cos(radians(theta2[2]+theta3[1]))
+    s4 = sin(radians(theta4[2]))
+    c4 = cos(radians(theta4[2]))
+    s5 = sin(radians(theta5[2]))
+    c5 = cos(radians(theta5[2]))
+    theta6.append(degrees(atan2(
+        (-r11*(c1*c23*s4-s1*c4)-(r21*(s1*c23*s4+c1*c4))+r31*s23*s4), (r11*((c1*c23*c4+s1*s4)*c5-c1*s23*s5))+(r21*((s1*c23*c4-c1*s4)*c5-s1*s23*s5))-(r31*(s23*c4*c5+c23*s5)))))
+    s1 = sin(radians(theta1[1]))
+    c1 = cos(radians(theta1[1]))
+    s23 = sin(radians(theta2[3]+theta3[1]))
+    c23 = cos(radians(theta2[3]+theta3[1]))
+    s4 = sin(radians(theta4[3]))
+    c4 = cos(radians(theta4[3]))
+    s5 = sin(radians(theta5[3]))
+    c5 = cos(radians(theta5[3]))
+    theta6.append(degrees(atan2(
+        (-r11*(c1*c23*s4-s1*c4)-(r21*(s1*c23*s4+c1*c4))+r31*s23*s4), (r11*((c1*c23*c4+s1*s4)*c5-c1*s23*s5))+(r21*((s1*c23*c4-c1*s4)*c5-s1*s23*s5))-(r31*(s23*c4*c5+c23*s5)))))
+
+    # creating a list of joint angles calculated above
+    joint_angles = []
+
+    joint_angles.append(
+        (theta1[0], theta2[0], theta3[0], theta4[0], theta5[0], theta6[0]))
+    joint_angles.append(
+        (theta1[1], theta2[1], theta3[0], theta4[1], theta5[1], theta6[1]))
+    joint_angles.append(
+        (theta1[0], theta2[2], theta3[1], theta4[2], theta5[2], theta6[2]))
+    joint_angles.append(
+        (theta1[1], theta2[3], theta3[1], theta4[3], theta5[3], theta6[3]))
+    joint_angles.append(
+        (theta1[0], theta2[0], theta3[0], theta4[0]+180, -theta5[0], theta6[0]+180))
+    joint_angles.append(
+        (theta1[1], theta2[1], theta3[0], theta4[1]+180, -theta5[1], theta6[1]+180))
+    joint_angles.append(
+        (theta1[0], theta2[2], theta3[1], theta4[2]+180, -theta5[2], theta6[2]+180))
+    joint_angles.append(
+        (theta1[1], theta2[3], theta3[1], theta4[3]+180, -theta5[3], theta6[3]+180))
+
+    print("\nPossible joint angles are: ")
+    for i in range(8):
+        print('theta1: {:.3f}, theta2: {:.3f}, theta3: {:.3f}, theta4: {:.3f}, theta5: {:.3f}, theta6: {:.3f}'.format(
+            joint_angles[i][0], joint_angles[i][1], joint_angles[i][2], joint_angles[i][3], joint_angles[i][4], joint_angles[i][5]))
+        print()
+
+
+def main():
+
+    print("Program to calculate forward and inverse kinematics of PUMA 560 robot.\n")
+    choice = input("Enter 1 for forward kinematics or 2 for inverse kinematics. Enter your choice: ")
+
+    if choice == '1': 
+        dh_table = input_fk()
+        solve_fk(dh_table)
+
+    elif choice == '2':
+        T, a2, a3, d3, d4 = input_ik()
+        solve_ik(T, a2, a3, d3, d4)
+
+    else:
+        print("Incorrect choice, exiting.")
 
 if __name__ == '__main__':
     main()
